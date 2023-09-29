429cbb59f898d51f4b445098967f7acf1cf879d9
Graham Spencer <gspencer447@yahoo.com>
2023-09-29 14:43:53 -0500

    completed exercises 1-5

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
-
-// Exercise 1
-
-// Exercise 2
+//Exercise 1 & 2
+const firstName = "Graham"
+let lastName = "Spencer"
+var age = 29
 
 // Exercise 3
+let language = "language"
+let createdYear = 1995
+let isCaseSensitive = true
+
+//Exercise 4
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+//Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
